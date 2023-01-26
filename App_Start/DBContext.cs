281c59cd72ed2d0b165632c1554f13ea7f85da0f
Rajeev2690 <rajeev.kumar2690@gmail.com>
2023-01-26 20:13:04 +0530

    first commit

---
+++ App_Start/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.App_Start
+{
+    public class DBContext
+    {
+
+    }
+}
